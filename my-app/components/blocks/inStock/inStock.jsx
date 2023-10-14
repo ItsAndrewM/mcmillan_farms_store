e5f1922c210cb1e9554198f1716957243bf8844e
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-10-14 10:11:12 -0700

    handles yellow

--- my-app/components/blocks/inStock/inStock.jsx
+++ my-app/components/blocks/inStock/inStock.jsx
@@ -3,33 +3,47 @@ import styles from "./inStock.module.css";
 import { useEffect, useState } from "react";
 
 const InStock = ({ product }) => {
-  const [message, setMessage] = useState();
+  const [message, setMessage] = useState("In stock, ready to ship");
 
   useEffect(() => {
-    const setStockMessage = () => {
-      let msg;
-      switch (product.stock_status) {
-        case "in_stock":
-          msg = "In stock, ready to ship";
-          break;
-        case "out_of_stock":
-          msg = "Out of stock, try again later";
-          break;
-      }
-      setMessage(msg);
-    };
-    setStockMessage();
-  }, []);
+    if (product.stock_level >= 3) {
+      setMessage(message);
+    } else if (product.stock_level < 3 && product.stock_level > 0) {
+      setMessage(`Low stock - ${product.stock_level} item left`);
+    } else {
+      setMessage("sold out");
+    }
+  }, [product]);
 
   if (!message) {
     return <LoadingDots />;
   }
 
+  if (message.includes("sold out")) {
+    return <></>;
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <span className={styles.ring}></span>
-        <span className={styles.circle}></span>
+        <span
+          className={styles.ring}
+          style={{
+            border: `${
+              product.stock_level >= 3
+                ? "3px solid #62bd19"
+                : "3px solid #f4af29"
+            }`,
+          }}
+        ></span>
+        <span
+          className={styles.circle}
+          style={{
+            backgroundColor: `${
+              product.stock_level >= 3 ? "#62bd19" : "#f4af29"
+            }`,
+          }}
+        ></span>
       </div>
       <small>{message}</small>
     </div>
